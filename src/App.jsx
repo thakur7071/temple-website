98f5cff5c94aab4f054fc7f2beaf89a355c2dcd1
thakur7071 <singhnaveen2370@gmail.com>
2025-03-11 23:00:29 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 // import AppRoutes from './components/routes/AppRoutes';
 import Home from "../src/pages/Home/Home"
+import Footer from './components/Footer/Footer';
 function App() {
   return (
     <>
       <Navbar />
       {/* <AppRoutes /> */}
       <Home/>
+      <Footer/>
     </>
   );
 }
