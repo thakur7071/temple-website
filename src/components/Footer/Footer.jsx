690b4fc165aeeff9b3cc14d1127517c86b2155a1
thakur7071 <singhnaveen2370@gmail.com>
2025-03-12 00:07:40 +0530

    Initial Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import "./Footer.css"
+import React from 'react';
+import './Footer.css';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-logo">
+          <h2>Temple Name</h2>
+          <p>Peace | Devotion | Spirituality</p>
+        </div>
+        <div className="footer-links">
+          <h2>Quick Links</h2>
+          <ul>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#donate">Donate</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+        <div className="footer-social">
+          <h2>Follow Us</h2>
+          <div className="social-icons">
+            <a href="#"><FaFacebook /></a>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaTwitter /></a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Temple Name. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
